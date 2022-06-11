43f02925a5b332ad246c43264a42108fb45eca9d
Mauri Di Pietro <dipietro.jm@gmail.com>
2022-06-11 11:52:45 -0300

    cors

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
+    next();
+  });
 const whiteList = ['http://localhost:3000', 'ws://localhost:3000', 'https://larueca.netlify.app/', 'https://larueca.vercel.app/']
 app.use(cors({origin: whiteList}));
 
