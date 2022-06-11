9f87e299777f89f9956913f140df47634c5bbfd3
Mauri Di Pietro <dipietro.jm@gmail.com>
2022-06-11 09:04:35 -0300

    deploy-errores

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
-const whiteList = ['http://localhost:3000', 'ws://localhost:3000', 'https://larueca.netlify.app/']
+const whiteList = ['http://localhost:3000', 'ws://localhost:3000', 'https://larueca.netlify.app/', 'https://larueca.vercel.app/']
 app.use(cors({origin: whiteList}));
 
 
